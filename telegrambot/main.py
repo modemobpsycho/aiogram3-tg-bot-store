ab6280584593aa28c2d546b5bead815541066963
Vadim <hawaiianwork@mail.ru>
2024-02-14 02:48:39 +0300

    basic

--- telegrambot/main.py
+++ telegrambot/main.py
@@ -1,4 +1,27 @@
-import aiogram
+from aiogram import Bot, Dispatcher, executor, types
 
 
 TOKEN_API = ""
+
+HELP_COMMAND = """
+/help - commands list
+/start - work with bot
+"""
+
+bot = Bot(TOKEN_API)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(command=["help"])
+async def help_command(message: types.Message):
+    await message.reply(text=HELP_COMMAND)
+
+
+@dp.message_handler(command=["start"])
+async def help_command(message: types.Message):
+    await message.answer(text="WEEEEEELCOME!!")
+    await message.delete()
+
+
+if __name__ == "__main__":
+    executor.start_polling(dp)
