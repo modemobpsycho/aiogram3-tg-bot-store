e11cdfb7483db934111def1f9cd8c2b626817341
Vadim <hawaiianwork@mail.ru>
2024-03-30 19:27:33 +0300

    update structure and del useless files, some pre-code

--- telegrambot/main.py
+++ telegrambot/main.py
@@ -1,34 +1,57 @@
 import asyncio
-import logging
+import os
 
-from aiogram import Bot, Dispatcher
-from config_data.config import Config, load_config
-from handlers import other_handlers, user_handlers
+from aiogram import Bot, Dispatcher, types
+from aiogram.enums import ParseMode
 
-logger = logging.getLogger(__name__)
+from dotenv import find_dotenv, load_dotenv
+
+load_dotenv(find_dotenv())
+
+from middlewares.db import DataBaseSession
+
+from database.engine import create_db, drop_db, session_maker
+
+from handlers.private_user_handlers import user_private_router
+from handlers.user_group import user_group_router
+from handlers.admin_handlers import admin_router
+
+# from common.bot_cmds_list import private
+
+
+# ALLOWED_UPDATES = ['message', 'edited_message', 'callback_query']
+
+bot = Bot(token=os.getenv("TOKEN"), parse_mode=ParseMode.HTML)
+bot.my_admins_list = []
+
+dp = Dispatcher()
+
+dp.include_router(user_private_router)
+dp.include_router(user_group_router)
+dp.include_router(admin_router)
+
+
+async def on_startup(bot):
+
+    # await drop_db()
+
+    await create_db()
+
+
+async def on_shutdown(bot):
+    print("бот лег")
 
 
 async def main():
-    logging.basicConfig(
-        level=logging.INFO,
-        format="%(filename)s:%(lineno)d #%(levelname)-8s "
-        "[%(asctime)s] - %(name)s - %(message)s",
-    )
+    dp.startup.register(on_startup)
+    dp.shutdown.register(on_shutdown)
 
-    logger.info("Starting bot")
-
-    config: Config = load_config()
-
-    bot = Bot(token=config.tg_bot.token, parse_mode="HTML")
-    dp = Dispatcher()
-
-    await set_main_menu(bot)
-
-    dp.include_router(user_handlers.router)
-    dp.include_router(other_handlers.router)
+    dp.update.middleware(DataBaseSession(session_pool=session_maker))
 
     await bot.delete_webhook(drop_pending_updates=True)
-    await dp.start_polling(bot)
+    # await bot.delete_my_commands(scope=types.BotCommandScopeAllPrivateChats())
+    # await bot.set_my_commands(commands=private, scope=types.BotCommandScopeAllPrivateChats())
+    await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
 
 
 asyncio.run(main())
